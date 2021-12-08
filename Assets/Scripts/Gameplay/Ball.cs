45d2cda4bcc0f04510331d368a09cc77c13e14da
Suriya <suriyak.dev@gmail.com>
2021-12-08 10:32:59 +0530

    Ball velocity on hitting wall and player is updated

--- Assets/Scripts/Gameplay/Ball.cs
+++ Assets/Scripts/Gameplay/Ball.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-
     [SerializeField]
     Vector3 initialImpulse;
 
-
+    int[] directions = new int[4];
+    
     Rigidbody ball_rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
+        directions[0] = 4;
+        directions[1] = -4;
+        directions[2] = 10;
+        directions[3] = -10;
+
+        initialImpulse.x = directions[Random.Range(0, 2)]; 
+        initialImpulse.z = directions[Random.Range(2, 4)]; 
+
         ball_rigidbody = this.gameObject.GetComponent<Rigidbody>();
 
         ball_rigidbody.AddForce(initialImpulse, ForceMode.Impulse); //For Initial ball moving force.
     }
 
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.name == "Player")
+        {
+            ball_rigidbody.AddForce(ball_rigidbody.velocity * 10 * Time.deltaTime, ForceMode.Impulse); //For Initial ball moving force.
+        }
+        else {
+            ball_rigidbody.AddForce(ball_rigidbody.velocity * 5 * Time.deltaTime , ForceMode.Impulse); //For Initial ball moving force.
+        }
     }
 
 }
